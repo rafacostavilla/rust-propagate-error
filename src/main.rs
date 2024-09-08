34ce273b2d0d01edd82ad6f1e0ff26850e0e6d25
rafacostavilla <rafaelcostavilla@gmail.com>
2024-09-08 18:15:09 +1200

    Change main signature to return Result<(), E> and create a funtion to return the last char of the first line using ?-operator

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use std::fs;
+use std::fs::{self, File};
+use std::error::Error;
 use std::io;
 
-fn main() {
+// main can return Result<(), E>
+fn main() -> Result<(), Box<dyn Error>> {
     let result = read_username_from_file();
     match result {
         Ok(username) => println!("{username}"),
         Err(error) => panic!("Error is: {error:?}"),
     }
+
+    // This does not compile unless we change main signature
+    let greeting_file = File::open("error.txt")?;
+    Ok(())
 }
 
 
@@ -34,10 +40,15 @@ fn read_username_from_file() -> Result<String, io::Error>{
     // Ok(username)
 
     // The shortest way to do this:
-    // let mut username = String::new();
+    // let mut username = String::new(); 
     // File::open("hello.txt")?.read_to_string(&mut username)?;
     // Ok(username)
 
     fs::read_to_string("hello.txt")
 
+}
+
+// Example of using the ?-operator with Option instead of Result type
+fn last_char_of_first_line(text: &str) -> Option<char>{
+    text.lines().next()?.chars().last()
 }
